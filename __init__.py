1fe498f964d7261138a0a2bffd896a4e14f3e94b
Techno-Simpola <ashukumarsinghrathore123@gmail.com>
2021-10-27 22:48:28 +0530

    password updated

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ import pymongo
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "9053ec8968f39c8c633ed333064468cad835e301"
-app.config["MONGO_URI"] = "mongodb+srv://Search-DB:shu12wedfcV@cluster0.igzqx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://Search-DB:password-glugledbcluster0.igzqx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 mongo_client = pymongo.MongoClient(app)
 db = mongo_client.db
