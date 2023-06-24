55acfd06b6c7b985aff332dd3cdfa7e4d94261e1
Techno-Simpola <rathoreashutosh3006@gmail.com>
2023-06-24 23:15:41 +0530

    Revert "adding venv"
    This reverts commit 4b6b7b29f01f925470309eda1c9477083da53423.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request
-# from dns import resolver
+from dns import resolver
 import pymongo
 from flask_paginate import Pagination, get_page_args
 import os
